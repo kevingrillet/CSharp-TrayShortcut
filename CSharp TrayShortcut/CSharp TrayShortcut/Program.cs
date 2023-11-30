df0637029ed2dd0498f43f420f02739b9556ea1f
kevingrillet <kevin.grillet@gmail.com>
2023-11-30 12:09:21 +0100

    log crash

--- CSharp TrayShortcut/CSharp TrayShortcut/Program.cs
+++ CSharp TrayShortcut/CSharp TrayShortcut/Program.cs
@@ -4,12 +4,29 @@ namespace CSharp_TrayShortcut
 {
     internal static class Program
     {
+        private static void CatchException
+            (object sender, ThreadExceptionEventArgs e)
+        {
+            try
+            {
+                StreamWriter sw = new("crash-" + DateTime.Now.ToString().Replace("/", "-").Replace(":", "-") + ".txt");
+                Exception ex = e.Exception;
+                sw.WriteLine(ex.Message + ex.StackTrace);
+                sw.Close();
+            }
+            finally
+            {
+                Application.Exit();
+            }
+        }
+
         [STAThread]
         private static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new TrayApplicationContext());
+            Application.ThreadException += new ThreadExceptionEventHandler(CatchException);
         }
     }
 }
